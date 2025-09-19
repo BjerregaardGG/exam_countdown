48059356e7a7b9c2d3a7dc11967fbf41ce486bc2
BjerregaardGG <oliverbjerregaard1@gmail.com>
2025-09-19 17:18:47 +0200

    warning sound

--- app.js
+++ app.js
@@ -1,32 +1,12 @@
 const express = require("express"); 
 const app = express(); 
 
-const eksamensDato = new Date("2026-01-12T14:30:45"); 
-const date = new Date(); 
-const dateAndTime = date.toLocaleString("da-DK"); 
-
-const diffMs = eksamensDato - date; 
-
-// 2. Convert til andre enheder
-const diffSeconds = diffMs / 1000;                    // 94500 sekunder
-const diffMinutes = diffMs / (1000 * 60);             // 1575 minutter  
-const diffHours = diffMs / (1000 * 60 * 60);          // 26.25 timer
-const diffDays = diffMs / (1000 * 60 * 60 * 24);      // 1.09 dage
-
-console.log(`Forskel: ${diffDays.toFixed(1)} dage`);
-
-console.log(dateAndTime); 
+app.use(express.static(__dirname + "/static"));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/static/index.html");
-    
-});
-
-
-app.get("/age_calculator", (req,res) => {
 
 });
-
 
 const PORT = 8080; 
 app.listen(PORT, () => {
